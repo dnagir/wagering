dcbb0cfe64ab6109ce13d027b60399096eb3cb10
Dmytrii Nagirniak <dnagir@gmail.com>
2010-08-30 01:25:28 +1000

    Just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wagering::Application.config.secret_token = '878b8c1909630e78d522be0999cfe8e37985bb78ab1031793b1ba3bfc7a56be07320935961ee7f1f9336950b9d335e3e55a0dd5bbc36c630244895d4a25c511d'
